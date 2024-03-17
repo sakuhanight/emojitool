bdf0373940151652e311d2a70d16bfb29fd0596c
sakuhanight <sakuha@tsuitachi.net>
2024-03-18 08:23:47 +0900

    mod: MultipartRequest 関連を request.go に移動

--- lib/request.go
+++ lib/request.go
@@ -5,7 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"go.uber.org/zap"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func Request(endpoint string, body interface{}) (*http.Response, error) {
@@ -36,4 +40,47 @@ func RequestRaw(endpoint string, contentType string, body *bytes.Buffer) (*http.
 
 	zap.S().Debugln("RequestRaw end")
 	return res, err
+}
+
+type MultipartRequestOption func(writer *multipart.Writer)
+
+func MultipartRequest(endpoint string, options ...MultipartRequestOption) (*http.Response, error) {
+	zap.S().Debugln("MultipartRequest called")
+	body := &bytes.Buffer{}
+	w := multipart.NewWriter(body)
+	for _, opt := range options {
+		opt(w)
+	}
+	err := w.Close()
+	if err != nil {
+		zap.S().Fatalf("MultipartRequest writer close failed: %+v", err)
+	}
+	zap.S().Debugln("MultipartRequest for end")
+
+	return RequestRaw(endpoint, w.FormDataContentType(), body)
+}
+
+func SetMultipartField(field string, data []byte) MultipartRequestOption {
+	return func(writer *multipart.Writer) {
+		part, _ := writer.CreateFormField(field)
+		_, err := part.Write(data)
+		if err != nil {
+			zap.S().Panicf("CreatePart Failed")
+		}
+	}
+}
+
+func SetMultipartFile(field string, path string) MultipartRequestOption {
+	return func(writer *multipart.Writer) {
+		filename := filepath.Base(path)
+		file, err := os.Open(path)
+		if err != nil {
+			zap.S().Panicf("Could not open '%s': %v", path, err)
+		}
+		part, _ := writer.CreateFormFile(field, filename)
+		_, err = io.Copy(part, file)
+		if err != nil {
+			zap.S().Panicf("CreatePart Failed")
+		}
+	}
 }
--- lib/upload.go
+++ lib/upload.go
@@ -1,14 +1,9 @@
 package lib
 
 import (
-	"bytes"
 	"fmt"
 	"go.uber.org/zap"
-	"io"
-	"mime/multipart"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func UploadExample(host string, token string, path string) (*http.Response, error) {
@@ -20,49 +15,4 @@ func UploadExample(host string, token string, path string) (*http.Response, erro
 	options = append(options, SetMultipartFile("file", path))
 
 	return MultipartRequest(endpoint, options...)
-}
-
-type (
-	MultipartRequestOption func(writer *multipart.Writer)
-)
-
-func MultipartRequest(endpoint string, options ...MultipartRequestOption) (*http.Response, error) {
-	zap.S().Debugln("MultipartRequest called")
-	body := &bytes.Buffer{}
-	w := multipart.NewWriter(body)
-	for _, opt := range options {
-		opt(w)
-	}
-	err := w.Close()
-	if err != nil {
-		zap.S().Fatalf("MultipartRequest writer close failed: %+v", err)
-	}
-	zap.S().Debugln("MultipartRequest for end")
-
-	return RequestRaw(endpoint, w.FormDataContentType(), body)
-}
-
-func SetMultipartField(field string, data []byte) MultipartRequestOption {
-	return func(writer *multipart.Writer) {
-		part, _ := writer.CreateFormField(field)
-		_, err := part.Write(data)
-		if err != nil {
-			zap.S().Panicf("CreatePart Failed")
-		}
-	}
-}
-
-func SetMultipartFile(field string, path string) MultipartRequestOption {
-	return func(writer *multipart.Writer) {
-		filename := filepath.Base(path)
-		file, err := os.Open(path)
-		if err != nil {
-			zap.S().Panicf("Could not open '%s': %v", path, err)
-		}
-		part, _ := writer.CreateFormFile(field, filename)
-		_, err = io.Copy(part, file)
-		if err != nil {
-			zap.S().Panicf("CreatePart Failed")
-		}
-	}
 }
